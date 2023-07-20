7b0a143d57f88ebf8831efddc8f9100764f52da4
smomtahe <85276162+smomtahe@users.noreply.github.com>
2023-07-20 08:14:16 -0700

    Create U-Net.py
    U-Net architecture for image segmentation on the "Carvana Image Masking Challenge" dataset, which is an open-access dataset available on : https://www.kaggle.com/c/carvana-image-masking-challenge

---
+++ U-Net.py
@@ -0,0 +1,131 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, concatenate
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import accuracy_score, classification_report
+
+# Set the paths to the dataset
+train_data_dir = 'carvana/train'
+validation_data_dir = 'carvana/valid'
+test_data_dir = 'carvana/test'
+
+# Define data generators for data augmentation
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+batch_size = 32
+
+# Load and preprocess data using generators
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(128, 128),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='input',
+    shuffle=True
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(128, 128),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='input',
+    shuffle=False
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(128, 128),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='input',
+    shuffle=False
+)
+
+# Define U-Net model
+def unet(input_size=(128, 128, 1)):
+    inputs = Input(input_size)
+    
+    # Encoder
+    conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    # Decoder
+    up6 = Conv2D(512, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(drop5))
+    merge6 = concatenate([drop4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
+
+    up7 = Conv2D(256, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv6))
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
+
+    up8 = Conv2D(128, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv7))
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
+
+    up9 = Conv2D(64, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv8))
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+    return model
+
+# Compile the model
+model = unet()
+model.compile(optimizer=Adam(learning_rate=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+epochs = 20
+history = model.fit(
+    train_generator,
+    steps_per_epoch=len(train_generator),
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=len(validation_generator)
+)
+
+# Evaluate the model on test data
+test_loss, test_accuracy = model.evaluate(test_generator, steps=len(test_generator))
+print(f"Test Accuracy: {test_accuracy}")
+
+# Save the model
+model.save('unet_model.h5')
